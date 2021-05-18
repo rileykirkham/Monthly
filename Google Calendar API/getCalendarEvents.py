983ab150a95ea74ceb0951c64001a0faf99edf13
rileykirkham <rileypkirkham@gmail.com>
2021-05-18 15:15:01 -0600

    Update getCalendarEvents.py
    Fixed a lot of tabs vs spaces inconsistencies.
    Fixed a bug in callEvents.
    firstOfMonth is now kept as a date when passed to last_day_of_month and is changed to isoformat after that

--- Google Calendar API/getCalendarEvents.py
+++ Google Calendar API/getCalendarEvents.py
@@ -10,19 +10,21 @@ from google.oauth2.credentials import Credentials
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
 def last_day_of_month(any_day):
-    # get close to the end of the month for any day, and add 4 days 'over'
-    next_month = any_day.replace(day=28) + datetime.timedelta(days=4)
-    # subtract the number of remaining 'overage' days to get last day of current month, or said programattically said, the previous day of the first of next month
-    return next_month - datetime.timedelta(days=next_month.day)
+	# get close to the end of the month for any day, and add 4 days 'over'
+	next_month = any_day.replace(day=28) + datetime.timedelta(days=4)
+	# subtract the number of remaining 'overage' days to get last day of current month, or said programattically said, the previous day of the first of next month
+	return next_month - datetime.timedelta(days=next_month.day)
 
 # Returns list of events for this month.
 def callEvents(service, selectedDate):
-	firstOfMonth = datetime.datetime(selectedDate.year, selectedDate.month, 1).isoformat() + 'Z'# 'Z' indicates UTC time
-	lastOfMonth = last_day_of_month(firstOfMonth).isoformat() + 'Z'# 'Z' indicates UTC time
+	firstOfMonth = datetime.datetime(selectedDate.year, selectedDate.month, 1)
+	lastOfMonth = (last_day_of_month(firstOfMonth)).isoformat() + 'Z'# 'Z' indicates UTC time
+	# Rewriting firstOfMonth to be in iso format
+	firstOfMonth = firstOfMonth.isoformat() + 'Z'
 	return service.events().list(calendarId='primary', timeMin= firstOfMonth,
-                                        timeMax = lastOfMonth,
-                                        singleEvents=True, 
-                                        orderBy='startTime').execute()
+										timeMax = lastOfMonth,
+										singleEvents=True, 
+										orderBy='startTime').execute()
 
 def incMonth(selectedDate):
 	# If December, move to next year
@@ -32,8 +34,9 @@ def incMonth(selectedDate):
 	else:
 		newYear = selectedDate.year
 		newMonth = selectedDate.month + 1
-	return selectedDate.replace(year = newYear, month = newMonth)
-		
+	newDate = selectedDate.replace(year = newYear, month = newMonth)
+	return newDate
+
 def decMonth(selectedDate):
 	# If January, move to prev year
 	if(selectedDate.month == 1):
@@ -42,56 +45,57 @@ def decMonth(selectedDate):
 	else:
 		newYear = selectedDate.year
 		newMonth = selectedDate.month - 1
-	return selectedDate.replace(year = newYear, month = newMonth)
+	newDate = selectedDate.replace(year = newYear, month = newMonth)
+	return newDate
 
 
 def main():
    # Returns a list of user's events for the current month.
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+	creds = None
+	# The file token.json stores the user's access and refresh tokens, and is
+	# created automatically when the authorization flow completes for the first
+	# time.
+	if os.path.exists('token.json'):
+		creds = Credentials.from_authorized_user_file('token.json', SCOPES)
 
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
+	# If there are no (valid) credentials available, let the user log in.
+	if not creds or not creds.valid:
+		if creds and creds.expired and creds.refresh_token:
+			creds.refresh(Request())
+		else:
+			flow = InstalledAppFlow.from_client_secrets_file(
+				'credentials.json', SCOPES)
+			creds = flow.run_local_server(port=0)
+		# Save the credentials for the next run
+		with open('token.json', 'w') as token:
+			token.write(creds.to_json())
 
-    service = build('calendar', 'v3', credentials=creds)
+	service = build('calendar', 'v3', credentials=creds)
 
-    # Call the Calendar API
-    currentDate = datetime.datetime.utcnow()
-    selectedDate = currentDate
+	# Call the Calendar API
+	currentDate = datetime.datetime.utcnow()
+	selectedDate = currentDate
 
-    # TODO -- Add in calls for next month/previous month
-    '''
+	# TODO -- Add in calls for next month/previous month
+	'''
 	if(IncButtonPressed):
 		selectedDate = incMonth(selectedDate)
 	if(decButtonPressed):
 		selectedDate = decMonth(selectedDate)
-    '''
+	'''
 
 
-    print("Getting events in " + selectedDate.strftime("%B"))
-    
-    events_result = callEvents(service, selectedDate)
-    events = events_result.get('items', [])
+	print("Getting events in " + selectedDate.strftime("%B"))
+	
+	events_result = callEvents(service, selectedDate)
+	events = events_result.get('items', [])
 
-    if not events:
-        print('No upcoming events found.')
-    for event in events:
-        start = event['start'].get('dateTime', event['start'].get('date'))
-        print(start, event['summary'])
+	if not events:
+		print('No upcoming events found.')
+	for event in events:
+		start = event['start'].get('dateTime', event['start'].get('date'))
+		print(start, event['summary'])
 
 
 if __name__ == '__main__':
-    main()
+	main()
